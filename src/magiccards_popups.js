285b6181294e766b7029202f75b91d520b81b786
Leo Izen <leo.izen@gmail.com>
2017-08-18 05:39:35 -0400

    script.js: add Use JQuery and rename script

---
+++ src/magiccards_popups.js
@@ -0,0 +1,88 @@
+//* TITLE magiccards.info Popups **//
+//* VERSION 0.1.1 **//
+//* DESCRIPTION    Creates visual popups for links to cards on magiccards.info **//
+//* DEVELOPER thebombzen **//
+//* FRAME false **//
+//* BETA true **//
+
+XKit.extensions.magiccards_popups = new Object({
+
+    addPopup: function($atag, image_source){
+        var $img = $("<img>");
+        $img.prop("src", image_source);
+        $img.css("padding", "0px");
+        $img.css("border",  "0px");
+        $img.css("width", "312px");
+        $img.css("height", "445px");
+        $img.css("position", "fixed");
+        $img.css("visibility", "hidden");
+        $img.css("z-index", "300");
+        $atag.addClass("magiccards_popups-tooltip");
+        $atag.hover(function(){
+            $(this).children("img").last().css("visibility", "visible");
+        }, function(){
+            $(this).children("img").last().css("visibility", "hidden");
+        })
+        $atag.append($img);
+        $atag.on("mousemove.magiccards_popups", function(event){
+            var x = event.clientX, y = event.clientY;
+            if (y > window.innerHeight / 2){
+                y -= 485;
+            }
+            $(this).children("img").last().css("top", (y + 20) + 'px');
+            $(this).children("img").last().css("left", (x + 20) + 'px');
+        });
+    },
+
+    addPopupsIfNecessary: function (){
+        if (XKit.interface.is_tumblr_page() !== true){
+            return;
+        }
+        $("a").each(function(){
+            var $atag = $(this);
+            if ($atag.hasClass("magiccards_popups-tooltip")){
+                return;
+            }
+            var old_href = $atag.prop("href"), theMatch, image_source, replaced;
+            if (old_href){
+                if (old_href.match(/^.*?\/\/magiccards.info\/.*?\/.*?\/.*?\.html$/)){
+                    theMatch = /^.*?\/\/magiccards.info\/(.*?)\/(.*?)\/(.*?)\.html$/.exec(old_href);
+                    image_source = "http://magiccards.info/scans/" + theMatch[2] + "/" + theMatch[1] + "/" + theMatch[3] + ".jpg";
+                    XKit.extensions.magiccards_popups.addPopup($atag, image_source);
+                } else if (old_href.match(/^http:\/\/t.umblr.com\/redirect\?z=http%3A%2F%2Fmagiccards\.info%2F.*?%2F.*?%2F.*?\.html(&.*)*$/)){
+                    replaced = /^http:\/\/t.umblr.com\/redirect\?z=(http%3A%2F%2Fmagiccards\.info%2F.*?%2F.*?%2F.*?\.html)(&.*)*$/.exec(old_href);
+                    theMatch = /^.*?\/\/magiccards.info\/(.*?)\/(.*?)\/(.*?)\.html$/.exec(decodeURIComponent(replaced[1]));
+                    image_source = "http://magiccards.info/scans/" + theMatch[2] + "/" + theMatch[1] + "/" + theMatch[3] + ".jpg";
+                    XKit.extensions.magiccards_popups.addPopup($atag, image_source);
+                }
+            }
+        });
+    },
+
+    removePopup: function($atag){
+        $atag.children("img").last().remove();
+        $atag.off("mousemove.magiccards_popups");
+        $atag.removeClass("magiccards_popups-tooltip");
+    },
+
+    removePopups: function(){
+        $(".magiccards_popups-tooltip").each(function () {
+             XKit.extensions.magiccards_popups.removePopup($(this));
+        });
+    },
+
+    running: false,
+
+    run: function() {
+        this.running = true;
+        XKit.extensions.magiccards_popups.addPopupsIfNecessary();
+        XKit.post_listener.add("magiccards_popups", XKit.extensions.magiccards_popups.addPopupsIfNecessary);
+    },
+
+    destroy: function() {
+        this.running = false;
+        XKit.extensions.magiccards_popups.removePopups();
+        XKit.post_listener.remove("magiccards_popups");
+    }
+
+});
