76e69dd0952e19e132752eb45704f798842358a9
Leo Izen <leo.izen@gmail.com>
2017-08-30 18:48:39 -0400

    script.js: Reorder functions

--- src/magiccards_popups.js
+++ src/magiccards_popups.js
@@ -7,30 +7,22 @@
 
 XKit.extensions.magiccards_popups = new Object({
 
-	addPopup: function($atag, image_source) {
-		var $img = $("<img>");
-		$img.prop("src", image_source);
-		$atag.addClass("magiccards_popups-tooltip");
-		$atag.hover(function() {
-			$(this).children("img").last().css("visibility", "visible");
-		}, function() {
-			$(this).children("img").last().css("visibility", "hidden");
-		});
-		$atag.append($img);
-		$atag.on("mousemove.magiccards_popups", XKit.extensions.magiccards_popups.moveToMouse($img));
+	running: false,
+
+	run: function() {
+		this.running = true;
+		if (XKit.interface.is_tumblr_page() === true) {
+			XKit.extensions.magiccards_popups.addPopupsIfNecessary();
+		}
+		XKit.tools.init_css("magiccards_popups");
+		XKit.post_listener.add("magiccards_popups", XKit.extensions.magiccards_popups.addPopupsIfNecessary);
 	},
 
-	moveToMouse: function($img) {
-		return function(event) {
-			var x = event.clientX, y = event.clientY;
-			if (y > window.innerHeight / 2) {
-				y -= 485;
-			}
-			$img.css({
-				"top": (y + 20) + 'px',
-				"left": (x + 20) + 'px'
-			});
-		}
+	destroy: function() {
+		this.running = false;
+		XKit.extensions.magiccards_popups.removePopups();
+		XKit.tools.remove_css("magiccards_popups");
+		XKit.post_listener.remove("magiccards_popups");
 	},
 
 	magiccards_regex: new RegExp("^http://magiccards.info/([^/]+)/([^/]+)/([^.]+).html"),
@@ -63,10 +55,30 @@ XKit.extensions.magiccards_popups = new Object({
 		}
 	},
 
-	removePopup: function($atag) {
-		$atag.children("img").last().remove();
-		$atag.off("mousemove.magiccards_popups");
-		$atag.removeClass("magiccards_popups-tooltip");
+	addPopup: function($atag, image_source) {
+		var $img = $("<img>");
+		$img.prop("src", image_source);
+		$atag.addClass("magiccards_popups-tooltip");
+		$atag.hover(function() {
+			$(this).children("img").last().css("visibility", "visible");
+		}, function() {
+			$(this).children("img").last().css("visibility", "hidden");
+		});
+		$atag.append($img);
+		$atag.on("mousemove.magiccards_popups", XKit.extensions.magiccards_popups.moveToMouse($img));
+	},
+
+	moveToMouse: function($img) {
+		return function(event) {
+			var x = event.clientX, y = event.clientY;
+			if (y > window.innerHeight / 2) {
+				y -= 485;
+			}
+			$img.css({
+				"top": (y + 20) + 'px',
+				"left": (x + 20) + 'px'
+			});
+		};
 	},
 
 	removePopups: function() {
@@ -75,22 +87,10 @@ XKit.extensions.magiccards_popups = new Object({
 		});
 	},
 
-	running: false,
-
-	run: function() {
-		this.running = true;
-		if (XKit.interface.is_tumblr_page() === true) {
-			XKit.extensions.magiccards_popups.addPopupsIfNecessary();
-		}
-		XKit.tools.init_css("magiccards_popups");
-		XKit.post_listener.add("magiccards_popups", XKit.extensions.magiccards_popups.addPopupsIfNecessary);
-	},
-
-	destroy: function() {
-		this.running = false;
-		XKit.extensions.magiccards_popups.removePopups();
-		XKit.tools.remove_css("magiccards_popups");
-		XKit.post_listener.remove("magiccards_popups");
+	removePopup: function($atag) {
+		$atag.children("img").last().remove();
+		$atag.off("mousemove.magiccards_popups");
+		$atag.removeClass("magiccards_popups-tooltip");
 	}
 
 });
